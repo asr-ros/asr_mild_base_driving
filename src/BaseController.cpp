0cc7fbfb345beba5845d9a07825f09810016c66e
Felix Marek <felixmarek93@googlemail.com>
2016-05-23 11:25:27 +0200

    higher average_size. Failure differenc.

--- src/BaseController.cpp
+++ src/BaseController.cpp
@@ -193,7 +193,7 @@ double BaseController::calculateAdapter(double required_velocity, double real_ve
 {
 
     double adapter_value = required_velocity / 200.f;
-    double difference = std::abs(required_velocity - real_velocity*100.f);
+    double difference = std::abs(required_velocity - (real_velocity*100.f+adapter));
     if(difference < 1)
     {
         if(required_velocity > 0){
